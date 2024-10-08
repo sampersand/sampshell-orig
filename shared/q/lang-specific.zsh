837ab92c74d3ec396615f167e5caf5035962730c
SamW <mail@sampersand.me>
2024-10-08 16:42:31 -0700

    remove q

--- shared/q/lang-specific.zsh
+++
@@ -1,7 +0,0 @@
-# Ruby-specific aliases
-alias rb=ruby
-alias bi='bundle install'
-alias bu='bundle update'
-
-alias cman='man -s 3'
-alias manc='man -s 3'
--- shared/q/spellcheck.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-#####################################
-# Fixing things I commonly misspell #
-#####################################
-
-alias gti=git # lol there's only one right nows
-
